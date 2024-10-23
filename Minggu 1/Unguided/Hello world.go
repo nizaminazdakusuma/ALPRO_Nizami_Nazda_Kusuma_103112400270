5fe2f14b7f469c85b058d64f33d9a1151c3d6e90
nizaminazdakusuma <nizami.nazda05@gmail.com>
2024-10-23 21:16:28 +0700

    Add files via upload
    Signed-off-by: nizaminazdakusuma <nizami.nazda05@gmail.com>

---
+++ Minggu 1/Unguided/Hello world.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main() {
+	var greetings = "Selamat datang di dunia DAP"
+	var a, b int
+
+	fmt.Println(greetings)
+	fmt.Scanln(&a, &b)
+	fmt.Printf("%v + %v = %v\n" , a, b, a+b)
+}
